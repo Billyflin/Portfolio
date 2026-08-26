e393dba105013b45576f8565ff78d6e2d139135a
Billy <billymartinezc@gmail.com>
2026-08-26 18:17:24 -0400

    Fix Linux deployment lockfile

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
@@ -17,7 +16,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   server: {
